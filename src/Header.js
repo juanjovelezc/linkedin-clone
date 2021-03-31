5c84cafab29698c0a3e36d1ea67fdc7532822643
Juanjo Vélez <juanjovc21@gmail.com>
2021-03-31 08:46:32 -0500

    de Trabajos a trabajo

--- src/Header.js
+++ src/Header.js
@@ -24,7 +24,7 @@ function Header() {
             <div className='header__right'>
                 <HeaderOption Icon={HomeIcon} title='Inicio'/>
                 <HeaderOption Icon={SupervisorAccountIcon} title='Mi cuenta'/>
-                <HeaderOption Icon={BusinessCenterIcon} title='Trabajos' />
+                <HeaderOption Icon={BusinessCenterIcon} title='Trabajo' />
                 <HeaderOption Icon={ChatIcon} title='Mensajes' />
                 <HeaderOption Icon={NotificationsIcon} title='Notificaciones' />
                 <HeaderOption avatar="https://z-p4-instagram.feoh3-1.fna.fbcdn.net/v/t51.2885-19/s150x150/130255906_2866638973658547_663171176675110769_n.jpg?_nc_ht=z-p4-instagram.feoh3-1.fna.fbcdn.net&_nc_ohc=9J6lh68eN8QAX9qq3Jd&tp=1&oh=dc5bfd063eafcf3aca47c7586560f7a4&oe=603C9B91" title='Me'/>
